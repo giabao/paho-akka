3fb93806e5868b2dc2c64e28ba558ad9dab3885b
Gia Bao <giabao@sandinh.net>
2016-05-22 15:12:20 +0700

    add ResubscribeSpec. See https://github.com/giabao/paho-akka/issues/2

---
+++ src/test/scala/com/sandinh/paho/akka/ResubscribeSpec.scala
@@ -0,0 +1,54 @@
+package com.sandinh.paho.akka
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestFSMRef, TestKit}
+import com.sandinh.paho.akka.MqttPubSub._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import sys.process._
+
+//https://github.com/giabao/paho-akka/issues/2
+class ResubscribeSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with Matchers
+    with BeforeAndAfterAll with ScalaFutures {
+
+  def this() = this(ActorSystem("ResubscribeSpec"))
+  override def afterAll() = TestKit.shutdownActorSystem(system)
+
+  "MqttPubSub" must {
+    "resubsribe after brocker restart" in {
+      val topic = "com.sandinh.paho.akka/ResubscribeSpec"
+
+      //MqttPubSub can operate even when broker not started
+      val pubsub = TestFSMRef(new MqttPubSub(PSConfig("tcp://localhost:1883")))
+      val subscribe = Subscribe(topic, self, 2)
+      pubsub ! subscribe
+      expectMsg(SubscribeAck(subscribe))
+
+      //MqttPubSub can receive Publish before connected to broker
+      val payload = "payload".getBytes("utf-8")
+      pubsub ! new Publish(topic, payload, 2)
+
+      //start broker
+      val p = "/usr/sbin/mosquitto".run()
+
+      val msg = expectMsgType[Message]
+      msg.topic shouldBe topic
+      msg.payload shouldEqual payload
+
+      //stop broker
+      p.destroy()
+
+      val payload2 = "payload2".getBytes("utf-8")
+      pubsub ! new Publish(topic, payload2, 2)
+
+      //then start broker again
+      val p2 = "/usr/sbin/mosquitto".run()
+
+      val msg2 = expectMsgType[Message]
+      msg2.topic shouldBe topic
+      msg2.payload shouldEqual payload2
+
+      p2.destroy()
+    }
+  }
+}
